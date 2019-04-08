eb48463b602759c554f9424edb2a8cb297ed2903
Drew <drew.distefano@me.com>
2019-04-08 13:56:35 -0500

    fixing but in PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/targitatx/shopping_cart_service.git',
       path: '/home/ubuntu/Code/targit/',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      // 'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
