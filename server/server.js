e5341f5daa49d045bf3d3c1c66a09fcf60511553
Drew <drew.distefano@me.com>
2019-04-09 11:29:57 -0500

    m

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('../dist'))
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(express.static('../dist'))
+
+// app.use(cors());
 
 const { getAllItems } = require('./database/helper_functions.js')
 const { updateItemQuantity } = require('./database/helper_functions.js')
